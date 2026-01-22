83ec80acb385ba106eea15587c93584d0d5009fb
Sagar kumar <panditsagar4995@gmail.com>
2026-01-22 13:34:25 +0530

    c

--- components/contact/ContactHero.jsx
+++ components/contact/ContactHero.jsx
@@ -26,7 +26,7 @@ export default function App() {
         <div className="max-w-[1400px] mx-auto relative z-50 w-full px-5 md:px-0 grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center mt-20 lg:mt-0">
           {/* Left Side: Text */}
           <div className="flex flex-col justify-start">
-            <h1 className="font-jakarta text-[2.65rem] md:text-7xl lg:text-[5.4rem] font-bold mt-16 sm:-mt-16 tracking-tight leading-none capitalize text-[#ECF5FF]">
+            <h1 className="font-jakarta text-[2.65rem] md:text-7xl lg:text-[5.4rem] font-bold mt-12 sm:-mt-16 tracking-tight leading-none capitalize text-[#ECF5FF]">
               Chat with the team{" "}
             </h1>
 
